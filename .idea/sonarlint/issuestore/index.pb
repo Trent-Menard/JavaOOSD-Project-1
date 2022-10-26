c2b315b3de87b4084e680f505296b314ba64be24
Trent Menard <trentmenard6@yahoo.com>
2022-10-26 16:26:02 -0500

    Convert to Maven project to hopefully resolve dependency issues

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,7 +0,0 @@
-
-Z
-*src/com/github/trentmenard/WeeklyShow.java,7\c\7c8b02da4ca487bf68ae361699b35b4554e44973
-Z
-*src/com/github/trentmenard/Collection.java,e\0\e03f1b4d16d49986e47346d85d2922e31cccdf44
-T
-$src/com/github/trentmenard/Main.java,0\0\00daff0c866244cc8b8322622a6415cc35c6308f
---
+++ src/main/java/com/github/trentmenard/Main.java
@@ -0,0 +1,23 @@
+package com.github.trentmenard;
+// Trent Menard
+
+import javax.swing.*;
+public class Main {
+
+    public static void main(String[] args) {
+        ShowCollection showCollection = new ShowCollection();
+
+        showCollection.readFromFile();
+
+        JFrame netflixFrame = new JFrame("Netflix");
+        netflixFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        NetflixPanel netflixPanel = new NetflixPanel(showCollection);
+        netflixFrame.setContentPane(netflixPanel.getNetflixPanel());
+
+        netflixFrame.setVisible(true);
+        netflixFrame.pack();
+
+        // TODO: 10/10/2022 MAYBE: Replace ArrayList w/ Hash/TreeSet to prevent duplicates.
+    }
+}
